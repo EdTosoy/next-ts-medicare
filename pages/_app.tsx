c8a00405387dcad4e828a9d3fb6daf8699c03f44
edtosoy <edberto.tosoy@gmail.com>
2021-08-31 21:18:22 +0800

    added medicare images, tailwind setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
 // _app.tsx
-import '@styles/globals.css';
+import 'tailwindcss/tailwind.css';
 
 import { FC } from 'react';
 import { AppProps } from 'next/app';
+import '@styles/globals.css';
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
   // eslint-disable-next-line react/jsx-props-no-spreading
